d0aa8e7483c7fc3b9802a3533255207d7cca6070
Quentin PERRET <quentin.perret@wiklog.fr>
2024-03-29 10:49:43 +0100

    publication ide helper

--- src/StarterKitServiceProvider.php
+++ src/StarterKitServiceProvider.php
@@ -6,6 +6,7 @@ use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Wiklog\StarterKit\Commands\PublishCommonJs;
 use Wiklog\StarterKit\Commands\PublishComponents;
+use Wiklog\StarterKit\Commands\PublishIdeHelper;
 use Wiklog\StarterKit\Commands\PublishInsights;
 use Wiklog\StarterKit\Commands\PublishLibrairiesMigration;
 use Wiklog\StarterKit\Commands\PublishPdfMulticell;
@@ -26,6 +27,7 @@ class StarterKitServiceProvider extends PackageServiceProvider
             // ->hasMigration('create_starter-kit_table')
             ->hasCommand(PublishCommonJs::class)
             ->hasCommand(PublishComponents::class)
+            ->hasCommand(PublishIdeHelper::class)
             ->hasCommand(PublishInsights::class)
             ->hasCommand(PublishLibrairiesMigration::class)
             ->hasCommand(PublishPdfMulticell::class)
