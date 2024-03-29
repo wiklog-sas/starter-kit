51653f96696302d00ca78bf8b675b04b4cb14e01
Quentin PERRET <quentin.perret@wiklog.fr>
2024-03-29 11:15:44 +0100

    fix cmd stubs

--- src/Commands/PublishStubs.php
+++ src/Commands/PublishStubs.php
@@ -27,17 +27,23 @@ class PublishStubs extends Command
     {
         $file_system = new Filesystem();
 
-        // Publish stubs files
+        // Publication des fichiers stubs
         $this->comment('Publications des fichiers stubs');
         $folder_origin = StarterKit::PATH_PUBLISH_STUBS.'stubs';
         $destination = base_path('stubs');
         $file_system->copyDirectory($folder_origin, $destination);
 
-        // Publish sh file
+        // Publication du fichier newModel.sh
         $this->comment('Publications du fichier sh');
         $file_origin = StarterKit::PATH_PUBLISH_STUBS.'sh/newModel.sh';
         $destination = base_path('newModel.sh');
         $file_system->copy($file_origin, $destination);
+
+        // Publication des fichiers de commandes
+        $this->comment('Publications des fichiers de commandes');
+        $folder_origin = StarterKit::PATH_PUBLISH_STUBS.'commands';
+        $destination = app_path('Console/Commands');
+        $file_system->copyDirectory($folder_origin, $destination);
 
         $this->comment('Publication réussi !');
 
