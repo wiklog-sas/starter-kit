579fddc356be3c75104ac68f2a904215cf028696
Quentin PERRET <quentin.perret@wiklog.fr>
2024-04-03 11:57:15 +0200

    fix publishDump

--- src/Commands/PublishDump.php
+++ src/Commands/PublishDump.php
@@ -32,14 +32,30 @@ class PublishDump extends Command
         $this->comment('Publication Dump');
         $file_origin = StarterKit::PATH_PUBLISH_DUMP.'dump/Dump.php';
         $destination = app_path('Classes/Dump/Dump.php');
+        $this->createDirIfNotExists(app_path('Classes/Dump'));
         $file_system->copy($file_origin, $destination);
 
         $file_origin = StarterKit::PATH_PUBLISH_DUMP.'command/DumpCommand.php';
         $destination = app_path('Console/Commands/DumpCommand.php');
+        $this->createDirIfNotExists(app_path('Console/Commands'));
         $file_system->copy($file_origin, $destination);
 
         $this->comment('Publication réussis !');
 
         return self::SUCCESS;
+    }
+
+    /**
+     * Créer un dossier s'il n'existe pas
+     *
+     * @param  string  $path
+     * @return void
+     */
+    public function createDirIfNotExists(string $path): void
+    {
+        $file_system = new Filesystem();
+        if (! $file_system->exists($path)) {
+            $file_system->makeDirectory($path, recursive: true);
+        }
     }
 }
